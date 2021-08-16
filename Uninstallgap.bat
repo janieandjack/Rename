d51db993822e24386fa99d37235ff91b7987669f
ellesaidh <46368730+ellesaidh@users.noreply.github.com>
2021-08-16 10:27:28 -0700

    Update Uninstallgap.bat

--- Uninstallgap.bat
+++ Uninstallgap.bat
@@ -6,15 +6,15 @@ MKDIR C:\Temp
 
 c:\windows\system32\ccmsetup\ccmsetup.exe /uninstall 
 
-timeout /t 1
+timeout /t 45
 
 msiexec /uninstall GlobalProtect64.msi /quiet
 
-timeout /t 1
+timeout /t 45
 
 "C:\Program Files (x86)\Juniper Networks\Junos Pulse\PulseUninstall.exe" /silent=1
 
-timeout /t 1
+timeout /t 45
 
 "C:\Program Files (x86)\Pulse Secure\Pulse\PulseUninstall.exe" /silent=1
 )
