a28f9451f62b0f4386e4ee8de94152180bfefbb8
ellesaidh <46368730+ellesaidh@users.noreply.github.com>
2021-08-16 07:59:04 -0700

    Add files via upload

---
+++ Uninstallgap.bat
@@ -0,0 +1,20 @@
+@echo off
+
+>c:\temp\uninstall.txt 2>&1 (
+
+MKDIR C:\Temp
+
+c:\windows\system32\ccmsetup\ccmsetup.exe /uninstall 
+
+timeout /t 1
+
+msiexec /uninstall GlobalProtect64.msi /quiet
+
+timeout /t 1
+
+"C:\Program Files (x86)\Juniper Networks\Junos Pulse\PulseUninstall.exe" /silent=1
+
+timeout /t 1
+
+"C:\Program Files (x86)\Pulse Secure\Pulse\PulseUninstall.exe" /silent=1
+)
