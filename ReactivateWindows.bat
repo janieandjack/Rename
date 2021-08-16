8ec9e6b642f2a184137faed3e0b1b019c0095744
ellesaidh <46368730+ellesaidh@users.noreply.github.com>
2021-08-16 11:00:38 -0700

    Update ReactivateWindows.bat

--- ReactivateWindows.bat
+++ ReactivateWindows.bat
@@ -4,9 +4,7 @@ cd c:\temp
 
 wmic path SoftwareLicensingService get OA3xOriginalProductKey > c:\temp\winkey.txt
 
-for /r %%a in (*.txt) do @type "%%a" |find "OA3xOriginalProductKey" /i /v >> "c:\temp\newwinkey.txt"
-
-FOR /F "delims=" %%a IN (c:\temp\newwinkey.txt) DO Set WK=%%a
+FOR /F "delims=" %%a IN (c:\temp\winkey.txt) DO Set WK=%%a
 
 Set "WINKEY=%WK%"
 
