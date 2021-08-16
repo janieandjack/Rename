9fb60854b526281d137c1f87bbbdb7f5b60defa0
ellesaidh <46368730+ellesaidh@users.noreply.github.com>
2021-08-16 11:09:57 -0700

    Update ReactivateWindows3.bat

--- ReactivateWindows3.bat
+++ ReactivateWindows3.bat
@@ -4,7 +4,7 @@ cd c:\temp
 
 wmic path SoftwareLicensingService get OA3xOriginalProductKey > c:\temp\winkey.txt
 
-for /F "delims=" %%i in (c:\temp\winkey.txt) do set "WK=%%i"
+for /F "delims=" %%i in (c:\temp\winkey.txt) do set WK=%%i
 
 Set "WINKEY=%WK%"
 
