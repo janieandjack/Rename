27acccf9f04d59e7b8ce143a8a5fa9731f308fae
ellesaidh <46368730+ellesaidh@users.noreply.github.com>
2021-08-16 17:37:19 -0700

    Add files via upload

---
+++ Map Network drives.ps1
@@ -0,0 +1,13 @@
+﻿$us = Read-Host 'Enter Your User Name:' -AsSecureString
+$pw = Read-Host 'Enter Your Password:' -AsSecureString
+
+
+cmdkey.exe /add:corpfs24 /user:gymboree\$us /pass:$pw
+cmdkey.exe /add:Corpfs20 /user:gymboree\$us /pass:$pw
+cmdkey.exe /add:Corp-fs-02 /user:gymboree\$us /pass:$pw
+cmdkey.exe /add:Corpmom1 /user:gymboree\$us /pass:$pw
+cmdkey.exe /add:Corpmfs0 /user:gymboree\$us /pass:$pw
+cmdkey.exe /add:Corppdm20 /user:gymboree\$us /pass:$pw
+cmdkey.exe /add:Corpvc21 /user:gymboree\$us /pass:%$pw
+cmdkey.exe /add:Corpqt21 /user:gymboree\$us /pass:$pw
+cmdkey.exe /add:Corpqt40-test /user:gymboree\$us /pass:$pw
