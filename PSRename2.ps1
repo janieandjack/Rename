39eb0cb4b93f2389700c76bbf7135bf4b5b28cfa
ellesaidh <46368730+ellesaidh@users.noreply.github.com>
2021-08-18 14:49:19 -0700

    Update and rename PSRename2.ps1 to PSRename3.ps1

--- PSRename2.ps1
+++
@@ -1,7 +0,0 @@
-﻿Get-WmiObject win32_bios | select Serialnumber | Out-File -FilePath c:\temp\SK.txt
-
-$serial = Get-Content c:\temp\SK.txt | Select -Index 3
-
-Rename-Computer -NewName "JJ-$serial" -Restart
-
-del c:\temp\SK.txt
---
+++ PSRename3.ps1
@@ -0,0 +1,7 @@
+﻿Get-WmiObject win32_bios | select Serialnumber | Out-File -FilePath c:\temp\SK.txt
+
+$serial = Get-Content c:\temp\SK.txt | Select -Index 3 | Format-Wide | Out-String).trim()
+
+#Rename-Computer -NewName "JJ-$serial" -Restart
+
+#del c:\temp\SK.txt
