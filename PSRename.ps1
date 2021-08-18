9a08b51eefaf6ef80fcdec17c199c6896495914e
ellesaidh <46368730+ellesaidh@users.noreply.github.com>
2021-08-18 14:34:10 -0700

    Add files via upload

---
+++ PSRename.ps1
@@ -0,0 +1,7 @@
+﻿Get-WmiObject win32_bios | select Serialnumber | Out-File -FilePath c:\temp\SK.txt
+
+$serial = Get-Content c:\temp\SK.txt | Select -Index 3
+
+Rename-Computer -NewName "JJ-ComputerName" -LocalCredential JJadmin -Restart
+
+del c:\temp\SK.txt
