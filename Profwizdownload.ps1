b715cee53f0559e06aa18b5884390d6eca71f5a9
ellesaidh <46368730+ellesaidh@users.noreply.github.com>
2021-08-20 10:18:30 -0700

    Add files via upload

---
+++ Profwizdownload.ps1
@@ -0,0 +1,8 @@
+﻿$path = "C:\temp\"
+    
+If (!(test-path $path))
+{
+    md $path
+}
+
+Invoke-WebRequest https://github.com/ellesaidh/Rename/blob/main/Profwiz.msi?raw=true -Outfile c:\Temp\Profwiz.msi
