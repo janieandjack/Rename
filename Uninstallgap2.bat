82dc905be1753a6bd23c02714964ccea72ff9a9a
ellesaidh <46368730+ellesaidh@users.noreply.github.com>
2021-09-30 07:45:01 -0700

    Update Uninstallgap2.bat

--- Uninstallgap2.bat
+++ Uninstallgap2.bat
@@ -4,12 +4,4 @@ MKDIR C:\Temp
 
 c:\windows\ccmsetup\ccmsetup.exe /uninstall 
 
-timeout /t 30
-
-"C:\Program Files (x86)\Pulse Secure\Pulse\PulseUninstall.exe" /silent=1
-
-timeout /t 30
-
-"%ProgramFiles%\Aruba Networks\ClearPassOnGuard\uninst.exe" -exit
-
 )
