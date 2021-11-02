15a4c1ff4df2f3b248a2bc33622ece57add7dece
ellesaidh <46368730+ellesaidh@users.noreply.github.com>
2021-11-02 16:19:56 -0700

    Add files via upload

---
+++ JJricoh.ps1
@@ -0,0 +1,11 @@
+﻿$path = "C:\temp\"
+    
+If (!(test-path $path))
+{
+    md $path
+}
+
+Invoke-WebRequest https://www.dropbox.com/s/32pfx8hjwkp2got/prntdrv.exe?dl=1 -Outfile c:\Temp\prntdrv.exe
+
+Echo off
+Start-Process -Wait -FilePath "c:\Temp\prntdrv.exe" -ArgumentList '/install -i 172.22.2.90 -l en -m "Pro C5200Sseries E-44B PS 1.0" -p " JJ Ricoh 225 Bush"' -PassThru
