6cdd15d6828ee9f1a87dc1fddd404d213060536a
ellesaidh <46368730+ellesaidh@users.noreply.github.com>
2021-11-03 13:07:33 -0700

    Update and rename JJRicohInstallDB2.ps1 to JJRicohInstallF.ps1

--- JJRicohInstallF.ps1
+++ JJRicohInstallF.ps1
@@ -5,7 +5,8 @@ If (!(test-path $path))
     md $path
 }
 
-Invoke-WebRequest https://www.dropbox.com/s/xlsbn742cjocrl9/prntdrv2.exe?dl=1 -Outfile c:\Temp\prntdrv.exe
+Invoke-WebRequest https://d1m2uyedaojrqy.cloudfront.net/FSO/ricoh/Pro1619/Wind116/cs-cz/v1.0/Ricoh_E-44B_v1_0RX_FD63_WinRel1.zip -Outfile c:\Temp\prntdrv.zip
 
-Echo off
+Expand-Archive -LiteralPath c:\Temp\prntdrv.zip -DestinationPath C:\Temp -Force
+
 Start-Process -Wait -FilePath "c:\Temp\prntdrv.exe" -ArgumentList '/install -i 172.22.2.90 -l en -m "Pro C5200Sseries E-44B PS 1.0" -p " JJ Ricoh 225 Bush"' -PassThru
