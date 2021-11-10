426b7a469442095d181987c1b56c226ab2defa50
ellesaidh <46368730+ellesaidh@users.noreply.github.com>
2021-11-09 19:46:41 -0800

    Update and rename JJRicohInstallFix.ps1 to JJRicohInstallFix2.ps1

--- JJRicohInstallFix2.ps1
+++ JJRicohInstallFix2.ps1
@@ -28,14 +28,14 @@ DO
 While (Get-Printer -Name "JJ Ricoh 225 Bush (Copy*)")
 
 ## if printer is already installed exit out of the script
-If (Get-Printer -Name "JJ Ricoh 225 Bush")
-{
-    exit 5
-}
+#If (Get-Printer -Name "JJ Ricoh 225 Bush")
+#{
+ #   exit 5
+#}
 
 ## Download the driver, expand the zip
-Invoke-WebRequest https://d1m2uyedaojrqy.cloudfront.net/FSO/ricoh/Pro1619/Wind116/cs-cz/v1.0/Ricoh_E-44B_v1_0RX_FD63_WinRel1.zip -Outfile c:\Temp\prntdrv.zip
-Expand-Archive -LiteralPath c:\Temp\prntdrv.zip -DestinationPath C:\Temp -Force
+#Invoke-WebRequest https://d1m2uyedaojrqy.cloudfront.net/FSO/ricoh/Pro1619/Wind116/cs-cz/v1.0/Ricoh_E-44B_v1_0RX_FD63_WinRel1.zip -Outfile c:\Temp\prntdrv.zip
+#Expand-Archive -LiteralPath c:\Temp\prntdrv.zip -DestinationPath C:\Temp -Force
 
 ## Installs Printer
-Start-Process -Wait -FilePath "c:\Temp\prntdrv.exe" -ArgumentList '/install -i 172.22.2.90 -l en -m "Pro C5200Sseries E-44B PS 1.0" -p "JJ Ricoh 225 Bush"' -PassThru
+#Start-Process -Wait -FilePath "c:\Temp\prntdrv.exe" -ArgumentList '/install -i 172.22.2.90 -l en -m "Pro C5200Sseries E-44B PS 1.0" -p "JJ Ricoh 225 Bush"' -PassThru
