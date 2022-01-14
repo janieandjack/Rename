8ef30055b76612400243630d1d666101b6ab362a
ellesaidh <46368730+ellesaidh@users.noreply.github.com>
2022-01-14 15:57:28 -0800

    Update and rename Profwizdownload.ps1 to credmandownload.ps1

--- credmandownload.ps1
+++ credmandownload.ps1
@@ -5,5 +5,4 @@ If (!(test-path $path))
     md $path
 }
 
-Invoke-WebRequest https://github.com/ellesaidh/Rename/blob/main/Profwiz.msi?raw=true -Outfile c:\Temp\Profwiz.msi
 Invoke-WebRequest https://raw.githubusercontent.com/ellesaidh/Rename/main/credmanager.bat -Outfile c:\Temp\credmanager.bat
