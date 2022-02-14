24ce06e3e43b1ef62bcbd506f5b85b9be9418aeb
ellesaidh <46368730+ellesaidh@users.noreply.github.com>
2022-02-14 14:15:57 -0800

    Update ADJoin4.ps1

--- ADJoin4.ps1
+++ ADJoin4.ps1
@@ -1,5 +1,5 @@
-$domain = $faronics1
-$password = $faronics2 | ConvertTo-SecureString -asPlainText -Force
-$username =  $faronics3 
+$domain = "jandj"
+$password = "Sw1ngwa55" | ConvertTo-SecureString -asPlainText -Force
+$username =  "jandj\doc" 
 $credential = New-Object System.Management.Automation.PSCredential($username,$password)
 Add-Computer -DomainName $domain -Credential $credential
