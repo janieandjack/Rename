4dcc83c1e487e3197cc1cc078c87409546af7d54
janieandjack <87084939+janieandjack@users.noreply.github.com>
2022-02-16 11:00:04 -0800

    Update localadminadd.bat

--- localadminadd.bat
+++ localadminadd.bat
@@ -1,2 +1,4 @@
-net user JJAdmin2 N3w8e91nNlNG5jjs /add /y
-net localgroup "Administrators" "JJAdmin2" /add /y
+set arg1=%arg1
+set arg2=%arg2
+net user %arg1% %arg2% /add /y
+net localgroup "Administrators" %aeg1% /add /y
