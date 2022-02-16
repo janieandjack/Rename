5d9bd5420de664a9da459e6a3bad334d1604ad8a
janieandjack <87084939+janieandjack@users.noreply.github.com>
2022-02-16 10:55:59 -0800

    Delete JoinAD.ps1

--- JoinAD.ps1
+++
@@ -1,5 +0,0 @@
-$Domain = 'janieandjack.com' ## put domain name here
-$password = "V1scount9!" | ConvertTo-SecureString -asPlainText -Force
-$username = "$janieandjack\doc" 
-$credential = New-Object System.Management.Automation.PSCredential($username,$password)
-Add-Computer -Domain $Domain -Credential $Credential
